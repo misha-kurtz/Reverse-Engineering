35507c7422fc36df1344f8e39dab851d51884d1f
misha-kurtz <misha.martin.kurtz@pm.me>
2026-05-25 10:07:51 -0400

    test 1 A06_3a

--- A06-Backdoor-Persistence-Service-Foothold/A06_1b/src/obj/Debug/net10.0/win-x64/src.AssemblyInfo.cs
+++ A06-Backdoor-Persistence-Service-Foothold/A06_1b/src/obj/Debug/net10.0/win-x64/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70af0c70202bf7f44273f43b9e5631b121716f71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880d04408155d5ddc0108769394347cdcd3d62e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
