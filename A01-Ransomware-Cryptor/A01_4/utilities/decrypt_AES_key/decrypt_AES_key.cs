8fc73be3c27e0febdbc4f4a62deacffe77ce8f8e
misha-kurtz <misha.martin.kurtz@pm.me>
2026-06-27 12:34:48 -0400

    test

--- A01-Ransomware-Cryptor/A01_4/utilities/decrypt_AES_key/decrypt_AES_key.cs
+++ A01-Ransomware-Cryptor/A01_4/utilities/decrypt_AES_key/decrypt_AES_key.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
@@ -10,9 +11,17 @@ class Program
     {
         string privateKeyXml = File.ReadAllText("C:\\Users\\misha.kurtz\\Reverse-Engineering\\A01-Ransomware-Cryptor\\A01_4\\utilities\\decrypt_AES_key\\A01_4_private_key.xml");
 
-        string postedValue = File.ReadAllText("C:\\Users\\misha.kurtz\\Reverse-Engineering\\A01-Ransomware-Cryptor\\A01_4\\utilities\\decrypt_AES_key\\posted_aesencrypted.txt").Trim();
+        string postedValue = File.ReadAllText("C:\\Users\\misha.kurtz\\Reverse-Engineering\\A01-Ransomware-Cryptor\\A01_4\\utilities\\decrypt_AES_key\\posted_aesencrypted.txt");
+
+        postedValue = WebUtility.UrlDecode(postedValue);
+        postedValue = postedValue.Trim();
+        postedValue = postedValue.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+
+        Console.WriteLine($"Base64 length: {postedValue.Length}");
 
         byte[] encryptedAesKey = Convert.FromBase64String(postedValue);
+
+        Console.WriteLine($"Ciphertext bytes: {encryptedAesKey.Length}");
 
         using var rsa = new RSACryptoServiceProvider(2048);
         rsa.FromXmlString(privateKeyXml);
@@ -20,7 +29,7 @@ class Program
         byte[] decryptedBytes = rsa.Decrypt(encryptedAesKey, false);
         string aesKey = Encoding.UTF8.GetString(decryptedBytes);
 
-        File.WriteAllText("A01_4_Recovered_AES_Key.txt", aesKey);
+        File.WriteAllText("C:\\Users\\misha.kurtz\\Reverse-Engineering\\A01-Ransomware-Cryptor\\A01_4\\utilities\\decrypt_AES_key\\A01_4_Recovered_AES_Key.txt", aesKey);
 
         Console.WriteLine($"Recovered AES key: {aesKey}");
     }
