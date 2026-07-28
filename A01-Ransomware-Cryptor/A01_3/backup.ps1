dac1301b7586d0f12f829aaefb6e82a370980661
misha-kurtz <misha.martin.kurtz@pm.me>
2026-07-28 09:08:36 -0400

    A01_3

--- A01-Ransomware-Cryptor/A01_3/backup.ps1
+++ A01-Ransomware-Cryptor/A01_3/backup.ps1
@@ -1,5 +1,12 @@
-$Source = "C:\Users\Public\A01_TestData"
-$Destination = "Z:\Backups\A01_TestData_Backup" # Updated to Z:
-$LogFile = "C:\ProgramData\BackupLogs\A01_TestData_Backup.log"
+$Source = "C:\Users\Public\A01_TestData\"
+$Destination = "Z:\Backups\A01_TestData_Backup\"
+$LogDir = "C:\ProgramData\BackupLogs"
+$LogFile = "$LogDir\A01_TestData_Backup.log"
 
+# Ensure the log directory exists before running Robocopy
+if (-not (Test-Path -Path $LogDir)) {
+    New-Item -ItemType Directory -Path $LogDir -Force | Out-Null
+}
+
+# Execute Robocopy
 robocopy $Source $Destination /MIR /COPYALL /ZB /R:3 /W:5 /LOG:$LogFile /TEE
